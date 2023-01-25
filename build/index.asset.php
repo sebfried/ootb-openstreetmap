b6a749beed16d4bafa062a6257df1c16b011fc1e
Sebastian Friedrich <dev@sebastianfriedrich.at>
2023-01-25 01:43:19 +0100

    changed stamen map design

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b02df17e5e9500f32cdc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '121f18de8551693a9da6');
